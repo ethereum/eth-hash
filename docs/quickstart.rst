8e150b7de6e92c7fba51d4eb68f39ec9949158ee
Piper Merriam <pipermerriam@gmail.com>
2018-02-20 13:02:22 -0700

    add capability to do incremental hash digests

--- docs/quickstart.rst
+++ docs/quickstart.rst
@@ -26,6 +26,18 @@ Compute a Keccak256 Hash
   >>> keccak(b'')
   b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"
 
+
+You may also compute hashes incrementally
+
+.. doctest::
+
+  >>> from eth_hash.auto import keccak
+  >>> digest = keccak.Digest(b'part-a')
+  >>> digest.update(b'part-b')
+  >>> digest.digest()
+  b'6\x91l\xdd50\xd6[\x7f\xf9B\xff\xc9SW\x98\xc3\xaal\xd9\xde\xdd6I\xb7\x91\x9e\xf4`pl\x08'
+
+
 Select one of many installed backends
 ---------------------------------------
 
--- eth_hash/main.py
+++ eth_hash/main.py
@@ -2,10 +2,31 @@ class Keccak256:
     def __init__(self, backend):
         self.hasher = backend.keccak256
 
-        assert self.hasher(b'') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
+        assert self.hasher(b'') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';\x7b\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
 
     def __call__(self, preimage):
         if not isinstance(preimage, bytes):
             raise TypeError("Can only compute the hash of a `bytes` value, not %r" % preimage)
 
         return self.hasher(preimage)
+
+    def Digest(self, part):
+        if not isinstance(part, bytes):
+            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % part)
+        digest = Digest(part, keccak_fn=self.hasher)
+        digest.update(part)
+        return digest
+
+
+class Digest:
+    def __init__(self, part, keccak_fn):
+        self.preimage_parts = []
+        self.keccak_fn = keccak_fn
+
+    def update(self, part):
+        if not isinstance(part, bytes):
+            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % part)
+        self.preimage_parts.append(part)
+
+    def digest(self):
+        return self.keccak_fn(b''.join(self.preimage_parts))
