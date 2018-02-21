82dc14761f90dd0da8498ea3336c6955f24ee469
Piper Merriam <pipermerriam@gmail.com>
2018-02-21 09:02:05 -0700

    Bump version: 0.1.0-alpha.3 → 0.1.0-alpha.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ extras_require['dev'] = (
 setup(
     name='eth-hash',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.3',
+    version='0.1.0-alpha.4',
     description="""eth-hash: The Ethereum hashing function, keccak256, sometimes (erroneously) called sha3""",
     long_description_markdown_filename='README.md',
     author='Jason Carver',
