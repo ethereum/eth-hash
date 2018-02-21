1fc4879b58e3e131432293693ca3f966c2c49998
Piper Merriam <piper@pipermerriam.com>
2018-02-21 08:02:47 -0700

    add capability to do incremental hash digests (#4)
    * add capability to do incremental hash digests
    
    * add ability to copy digests
    
    * change to use  and

--- eth_hash/backends/pysha3.py
+++ eth_hash/backends/pysha3.py
@@ -1,7 +1,7 @@
 from sha3 import (
-    keccak_256,
+    keccak_256 as _keccak_256,
 )
 
 
 def keccak256(prehash: bytes) -> bytes:
-    return keccak_256(prehash).digest()
+    return _keccak_256(prehash).digest()
--- eth_hash/main.py
+++ eth_hash/main.py
@@ -2,10 +2,33 @@ class Keccak256:
     def __init__(self, backend):
         self.hasher = backend.keccak256
 
-        assert self.hasher(b'') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
+        assert self.hasher(b'') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';\x7b\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
 
     def __call__(self, preimage):
         if not isinstance(preimage, bytes):
             raise TypeError("Can only compute the hash of a `bytes` value, not %r" % preimage)
 
         return self.hasher(preimage)
+
+    def new(self, part):
+        if not isinstance(part, bytes):
+            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % part)
+        return PreImage(part, keccak_fn=self.hasher)
+
+
+class PreImage:
+    def __init__(self, *parts, keccak_fn):
+        self.preimage_parts = list(parts)
+        self.keccak_fn = keccak_fn
+
+    def update(self, part):
+        if not isinstance(part, bytes):
+            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % part)
+        self.preimage_parts.append(part)
+
+    def digest(self):
+        return self.keccak_fn(b''.join(self.preimage_parts))
+
+    def copy(self):
+        digest = type(self)(*self.preimage_parts, keccak_fn=self.keccak_fn)
+        return digest
