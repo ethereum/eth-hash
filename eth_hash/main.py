dadfc1dac837996361329f4b585c11b11cdfa63f
Paul Lange <palango@gmx.de>
2018-02-27 17:32:50 +0100

    Support hashing of bytearrays

--- eth_hash/main.py
+++ eth_hash/main.py
@@ -5,14 +5,18 @@ class Keccak256:
         assert self.hasher(b'') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';\x7b\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
 
     def __call__(self, preimage):
-        if not isinstance(preimage, bytes):
-            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % preimage)
+        if not isinstance(preimage, (bytes, bytearray)):
+            raise TypeError(
+                "Can only compute the hash of `bytes` or `bytearray` values, not %r" % preimage
+            )
 
         return self.hasher(preimage)
 
     def new(self, part):
-        if not isinstance(part, bytes):
-            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % part)
+        if not isinstance(part, (bytes, bytearray)):
+            raise TypeError(
+                "Can only compute the hash of `bytes` or `bytearray` values, not %r" % part
+            )
         return PreImage(part, keccak_fn=self.hasher)
 
 
@@ -22,8 +26,10 @@ class PreImage:
         self.keccak_fn = keccak_fn
 
     def update(self, part):
-        if not isinstance(part, bytes):
-            raise TypeError("Can only compute the hash of a `bytes` value, not %r" % part)
+        if not isinstance(part, (bytes, bytearray)):
+            raise TypeError(
+                "Can only compute the hash of `bytes` or `bytearray` values, not %r" % part
+            )
         self.preimage_parts.append(part)
 
     def digest(self):
