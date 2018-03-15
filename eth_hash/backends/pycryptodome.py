3b930ee25fef56d8d4d73240d00cf90636a3b689
Piper Merriam <pipermerriam@gmail.com>
2018-03-15 11:39:19 -0600

    Change how preimage works to improve performance

--- eth_hash/backends/pycryptodome.py
+++ eth_hash/backends/pycryptodome.py
@@ -1,9 +1,32 @@
 from Crypto.Hash import (
     keccak,
 )
+from eth_hash.preimage import (
+    BasePreImage,
+)
 
 
 def keccak256(prehash: bytes) -> bytes:
-    hasher = keccak.new(digest_bits=256)
-    hasher.update(prehash)
+    hasher = keccak.new(data=prehash, digest_bits=256)
     return hasher.digest()
+
+
+class preimage(BasePreImage):
+    _hash = None
+
+    def __init__(self, prehash) -> None:
+        self._hash = keccak.new(data=prehash, digest_bits=256)
+        # pycryptodome doesn't expose a `copy` mechanism for it's hash objects
+        # so we keep a record of all of the parts for when/if we need to copy
+        # them.
+        self._parts = [prehash]
+
+    def update(self, prehash) -> None:
+        self._hash.update(prehash)
+        self._parts.append(prehash)
+
+    def digest(self) -> bytes:
+        return self._hash.digest()
+
+    def copy(self) -> 'preimage':
+        return preimage(b''.join(self._parts))
--- eth_hash/main.py
+++ eth_hash/main.py
@@ -1,10 +1,20 @@
+from typing import (
+    Union,
+)
+
+from .preimage import (
+    BasePreImage,
+)
+
+
 class Keccak256:
     def __init__(self, backend):
         self.hasher = backend.keccak256
+        self.preimage = backend.preimage
 
         assert self.hasher(b'') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';\x7b\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
 
-    def __call__(self, preimage):
+    def __call__(self, preimage: Union[bytes, bytearray]) -> bytes:
         if not isinstance(preimage, (bytes, bytearray)):
             raise TypeError(
                 "Can only compute the hash of `bytes` or `bytearray` values, not %r" % preimage
@@ -12,29 +22,9 @@ class Keccak256:
 
         return self.hasher(preimage)
 
-    def new(self, part):
-        if not isinstance(part, (bytes, bytearray)):
+    def new(self, preimage: (Union[bytes, bytearray])) -> BasePreImage:
+        if not isinstance(preimage, (bytes, bytearray)):
             raise TypeError(
-                "Can only compute the hash of `bytes` or `bytearray` values, not %r" % part
+                "Can only compute the hash of `bytes` or `bytearray` values, not %r" % preimage
             )
-        return PreImage(part, keccak_fn=self.hasher)
-
-
-class PreImage:
-    def __init__(self, *parts, keccak_fn):
-        self.preimage_parts = list(parts)
-        self.keccak_fn = keccak_fn
-
-    def update(self, part):
-        if not isinstance(part, (bytes, bytearray)):
-            raise TypeError(
-                "Can only compute the hash of `bytes` or `bytearray` values, not %r" % part
-            )
-        self.preimage_parts.append(part)
-
-    def digest(self):
-        return self.keccak_fn(b''.join(self.preimage_parts))
-
-    def copy(self):
-        digest = type(self)(*self.preimage_parts, keccak_fn=self.keccak_fn)
-        return digest
+        return self.preimage(preimage)
