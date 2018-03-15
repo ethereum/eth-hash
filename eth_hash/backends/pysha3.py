b2fb20bae182d27ef0bbb368b49386799b275a72
Piper Merriam <piper@pipermerriam.com>
2018-03-15 12:22:06 -0600

    Change how preimage works to improve performance (#11)

--- eth_hash/backends/pysha3.py
+++ eth_hash/backends/pysha3.py
@@ -1,3 +1,6 @@
+from eth_hash.preimage import (
+    BasePreImage,
+)
 from sha3 import (
     keccak_256 as _keccak_256,
 )
@@ -5,3 +8,21 @@ from sha3 import (
 
 def keccak256(prehash: bytes) -> bytes:
     return _keccak_256(prehash).digest()
+
+
+class preimage(BasePreImage):
+    _hash = None
+
+    def __init__(self, prehash) -> None:
+        self._hash = _keccak_256(prehash)
+
+    def update(self, prehash) -> None:
+        return self._hash.update(prehash)
+
+    def digest(self) -> bytes:
+        return self._hash.digest()
+
+    def copy(self) -> 'preimage':
+        dup = preimage(b'')
+        dup._hash = self._hash.copy()
+        return dup
