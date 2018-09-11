ef571a313a61a79ac4f2ce2503134fd3da9c0a94
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-09-11 17:39:14 +0300

    Fix #25 test_import unit tests

--- tests/core/test_import.py
+++ tests/core/test_import.py
@@ -1,17 +1,34 @@
 import pytest
+import sys
+from unittest import (
+    mock,
+)
+
+
+def clean_module(name):
+    """Clean a module import so all variables in it will be created again"""
+    try:
+        del sys.modules[name]
+    except KeyError:
+        pass
+    return
 
 
 def test_import_auto():
+    clean_module("eth_hash.auto")
     from eth_hash.auto import keccak  # noqa: F401
 
 
-def test_import_auto_empty_crash():
+def test_import_auto_empty_crash(monkeypatch):
+    clean_module("eth_hash.auto")
     from eth_hash.auto import keccak
-    with pytest.raises(ImportError, match="None of these hashing backends are installed"):
-        keccak(b'')
+    with mock.patch.dict("sys.modules", {"sha3": None, "Crypto.Hash": None}):
+        with pytest.raises(ImportError, match="None of these hashing backends are installed"):
+            keccak(b'eh')
 
 
 def test_import():
+    clean_module("eth_hash")
     import eth_hash  # noqa: F401
 
 
@@ -23,10 +40,12 @@ def test_import():
     ],
 )
 def test_load_by_env(monkeypatch, backend):
+    clean_module("eth_hash.auto")
     from eth_hash.auto import keccak
     monkeypatch.setenv('ETH_HASH_BACKEND', backend)
-    with pytest.raises(ImportError) as excinfo:
-        keccak(b'triggered')
+    with mock.patch.dict("sys.modules", {"sha3": None, "Crypto.Hash": None}):
+        with pytest.raises(ImportError) as excinfo:
+            keccak(b'triggered')
     expected_msg = (
         "The backend specified in ETH_HASH_BACKEND, '{0}', is not installed. "
         "Install with `pip install eth-hash[{0}]`.".format(backend)
@@ -35,6 +54,7 @@ def test_load_by_env(monkeypatch, backend):
 
 
 def test_load_unavailable_backend_by_env(monkeypatch):
+    clean_module("eth_hash.auto")
     from eth_hash.auto import keccak
     backend = 'this-backend-will-never-exist'
     monkeypatch.setenv('ETH_HASH_BACKEND', backend)
