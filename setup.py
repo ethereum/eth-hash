d286ff81ea5c4ff62dd09414cc6577e067ca0bdf
Jason Carver <ut96caarrs@snkmail.com>
2021-01-21 11:52:10 -0800

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open('./README.md') as readme:
 setup(
     name='eth-hash',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.3.0',
+    version='0.3.1',
     description="""eth-hash: The Ethereum hashing function, keccak256, sometimes (erroneously) called sha3""",
     long_description=long_description,
     long_description_content_type='text/markdown',
