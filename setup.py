a83ef0765895fa416398d3187a05e49ff9fa73c8
kclowes <kclowes@users.noreply.github.com>
2022-06-30 11:13:00 -0600

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
